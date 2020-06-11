fc93ab01aa4415c8293f25487f04411743c37074
hjorthjort <rikard.hjort@gmail.com>
2020-06-11 13:49:00 +0200

    Highlight '.'

--- k-framework/local/k3-mode/k3-mode.el
+++ k-framework/local/k3-mode/k3-mode.el
@@ -33,7 +33,7 @@
                  "sort" "op" "subsort" "rule" "context" "eq" "ceq" "load" "when" "require" "configuration" "context" "requires" "ensures"))
 
 ;; Set up the regexes
-(setq k-syntax-terminals-regex "\\.\\.\\.\\|~>\\||->\\|`\\w+"
+(setq k-syntax-terminals-regex "\\.\\.\\.\\|~>\\||->\\|.\\|`\\w+"
       k-keywords-regex (concat (regexp-opt k-keywords 'words) "::=\\||")
       k-declarations "\\(syntax\\|sort\\|op\\) \\([a-zA-Z{}\\-]+\\)"
       k-rewrites-regex "=>\\|<[^ ]+>")
