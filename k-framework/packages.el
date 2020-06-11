f4bfb3139e8fc295f8d53e93c40995d23e7763ca
hjorthjort <rikard.hjort@gmail.com>
2020-06-11 19:26:33 +0200

    Autoo-add support for K syntax hihglighting in Markdown

--- k-framework/packages.el
+++ k-framework/packages.el
@@ -3,6 +3,7 @@
 (setq k-framework-packages
   '(
     (k3-mode :location local)
+    markdown-mode
    )
 )
 
@@ -11,7 +12,10 @@
     :defer t
     :mode "\\.k\\'"
   )
-
+  (use-package markdown-mode
+    :config
+    (setq markdown-code-lang-modes (cons '("k" . k3-mode) markdown-code-lang-modes))
+    )
 )
 
 (defun k-framework/post-init-k3-mode ()
